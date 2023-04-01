2d628ae0f50abeb5819b0eb9da9a396290ce1c5e
Alexis Pister <alexis.pister@hotmail.com>
2023-04-02 00:43:51 +0200

    Updates

--- _app/immutable/chunks/2.7f40c16d.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page.a04cbb59.js";import{default as t}from"../entry/_page.svelte.fc484719.js";export{t as component,r as universal};
---
+++ _app/immutable/entry/_page.ts.760e39a3.js
@@ -0,0 +1,1 @@
+import"../chunks/paths.3b6f251c.js";import{l}from"../chunks/_page.0fcf53a4.js";export{l as load};
